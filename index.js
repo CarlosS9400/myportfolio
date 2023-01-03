f0b0be30104a135708f336c1baf1166c1ebc1b23
CarlosS9400 <100578104+CarlosS9400@users.noreply.github.com>
2023-01-03 21:37:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const menuImage = document.getElementById("menuImage")
+const menu = document.getElementById("menu")
+
+
+
+menuImage.addEventListener("click", function(){
+
+    
+    menu.classList.toggle("showMenu")
+})
+
+
+
