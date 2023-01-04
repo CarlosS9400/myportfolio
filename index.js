88cc5e70530dfcc91ca86d70e7571349dafd6a1d
CarlosS9400 <100578104+CarlosS9400@users.noreply.github.com>
2023-01-04 18:20:42 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const menuImage = document.getElementById("menuImage")
+const closeImage = document.getElementById("closeImage")
 const menu = document.getElementById("menu")
 
 
 
 menuImage.addEventListener("click", function(){
 
-    
     menu.classList.toggle("showMenu")
+    closeImage.style.display = "block"
+})
+
+closeImage.addEventListener("click", function(){
+  
+    menu.classList.toggle("showMenu")
+    menu.style.zIndex = 999999;
+    closeImage.style.display = "none"
 })
 
 
